ad6b50cfdae18ae4f5e74efa22bc4ffc6e5493e7
jakabeduard <79199568+jakabeduard@users.noreply.github.com>
2023-12-04 05:09:41 +0200

    fuufufufufuufu
    fuuuuuuuuuu

--- App/FrissKert_App/App.js
+++ App/FrissKert_App/App.js
@@ -1,43 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import {ImageBackground, StyleSheet, Text, View } from 'react-native';
-
+import React from 'react';
+import Eladok from '../FrissKert_App/screens/Eladok';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-    
-   
-    <ImageBackground source={require('../../App/FrissKert_App/assets/hatter.png')} resizeMode="cover" style={styles.image}>
-  
-      <Text style={styles.text}>
-
-      Frisgjskert</Text>
-      
-    </ImageBackground>
-  </View>
+    <Eladok />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  header:{
-
-    padding: 50,
-  },
-  image: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'white',
-    fontSize: 80,
-    lineHeight: 84,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    
-    
-  },
-});
-
---
+++ App/FrissKert_App/screens/Eladok.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function Eladok() {
+  return (
+    <View style={styles.container}>
+      <Text>Eladok</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+});
