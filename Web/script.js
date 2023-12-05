8113c3d486881657a7dee6e31cd58a02909451f4
PapRolandLevente <pap.roland.levente@student.ms.sapientia.ro>
2023-12-05 20:45:54 +0200

    login2 popup

--- Web/script.js
+++ Web/script.js
@@ -1,21 +1,29 @@
 const box = document.querySelector('.box');
 const registraciolink = document.querySelector('.regisztracio-link');
 const bejelentkezeslink = document.querySelector('.bejelentkezes-link');
+const btnPopup = document.querySelector('.btnLogin');
+const iconClose = document.querySelector('.icon_close');
+
 
 registraciolink.addEventListener('click', () => {
     box.classList.add('active');
-    console.log("vsfd");
 });
 
 bejelentkezeslink.addEventListener('click', () => {
     box.classList.remove('active');
 });
 
+btnPopup.addEventListener('click', () => {
+    box.classList.add('activepopup');
+});
 
+iconClose.addEventListener('click', () => {
+    box.classList.remove('activepopup');
+});
 
-  
-
-       
+iconClose.addEventListener('click', () => {
+    box.classList.remove('active');
+});   
      
       
 
