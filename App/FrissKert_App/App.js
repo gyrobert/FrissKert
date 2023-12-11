a0df5d4e07675458f1b946ec7b956e6b917befba
jakabeduard <79199568+jakabeduard@users.noreply.github.com>
2023-12-11 17:23:57 +0200

    20231211v2
    20231211v2

--- App/FrissKert_App/App.js
+++ App/FrissKert_App/App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StackNavigator from './StackNavigator';
+
+
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      <Text>Open up Apyour app!</Text>
+    
+      <Text>Open uyour app!</Text>
+
+      <StackNavigator/>
+
       <StatusBar style="auto" />
     </View>
   );
