5963a73aa4d7f9f33b63bbfd9bef8e32aaba4ae0
gyrobert <79217188+gyrobert@users.noreply.github.com>
2023-12-23 21:11:12 +0200

    Product

--- Web/script.js
+++ Web/script.js
@@ -5,13 +5,7 @@ const btnPopup = document.querySelector('.btnLogin');
 const iconClose = document.querySelector('.icon_close');
 
 
-registraciolink.addEventListener('click', () => {
-    box.classList.add('active');
-});
 
-bejelentkezeslink.addEventListener('click', () => {
-    box.classList.remove('active');
-});
 
 btnPopup.addEventListener('click', () => {
     box.classList.add('activepopup');
